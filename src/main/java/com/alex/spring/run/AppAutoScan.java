828cd11b0428be232f377036e16835cdbe18c3ef
Alexandr Ivanov <alex.ivanoff85@gmail.com>
2013-08-29 10:30:04 +0300

    Changing

---
+++ src/main/java/com/alex/spring/run/AppAutoScan.java
@@ -0,0 +1,18 @@
+package com.alex.spring.run;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.alex.spring.beans.Performer;
+
+public class AppAutoScan {
+
+	public static void main(String[] args) throws Exception {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("annoscan.xml");
+		
+		Performer perf = (Performer) ctx.getBean("eric");
+		perf.perform();
+
+	}
+
+}
