3401eb10767483d06dbc4ee87b1fed87b97dfef0
Alexandr Ivanov <alex.ivanoff85@gmail.com>
2013-08-29 11:59:01 +0300

    Delete AppLoggin

--- src/main/java/com/alex/spring/run/AppAutoScan.java
+++ src/main/java/com/alex/spring/run/AppAutoScan.java
@@ -8,6 +8,7 @@ import com.alex.spring.beans.Performer;
 public class AppAutoScan {
 
 	public static void main(String[] args) throws Exception {
+		@SuppressWarnings("resource")
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("annoscan.xml");
 		
 		Performer perf = (Performer) ctx.getBean("eric");
